f5fed248219168ff0c6ba45ec1835dda85e1619c
BOBSTRONK <caiweidong123@gmail.com>
2024-06-03 14:39:31 +0200

    local change

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.white,
         useMaterial3: true,
       ),
-      home: const VotingPage(),
+      home: const Home(),
     );
   }
 }
